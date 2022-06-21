feba914e9352ac46899c4787c3842c392e345d8c
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-06-21 12:50:38 +0300

    update konsta to latest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   build: {
+    transpile: ['konsta'],
     postcss: {
       postcssOptions: require('./postcss.config.js'),
     },
-  }
+  },
 })
